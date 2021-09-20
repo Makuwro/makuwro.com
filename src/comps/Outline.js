6ad078f1fcf4d2746da9bad9c64446575568a783
Christian Toney <christian@makuwro.com>
2021-09-20 10:19:47 -0400

    add key

--- src/comps/Outline.js
+++ src/comps/Outline.js
@@ -6,7 +6,7 @@ function Outline(props) {
 
   // Check if we have any content and set up the headers
   return props.headers ? <div id="outline">
-    <nav>{props.headers.map(item => React.createElement(item.type, null, item.props.children))}</nav>
+    <nav>{props.headers.map((item, index) => React.createElement(item.type, {key: index}, item.props.children))}</nav>
   </div> : null;
 
 }
