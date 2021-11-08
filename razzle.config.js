07fa430313a05a76af45dd53f359babd40e98567
Christian Toney <christian@makuwro.com>
2021-11-07 20:16:17 -0500

    let's try something

--- razzle.config.js
+++ razzle.config.js
@@ -3,13 +3,5 @@
 module.exports = {
   options: {
     buildType: "spa",
-  },
-  modifyPaths({paths}) {
-
-    paths.appBuildPublic = paths.appBuild;
-    paths.appBuildStaticExportRoutes = `${paths.appBuild}\\static_routes.js`;
-    console.log(paths);
-    return paths;
-
   }
 };
