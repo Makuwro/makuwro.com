3ca75c96e69f11f41b3f002570ea470faa86f841
Christian Toney <christian@makuwro.com>
2022-01-16 21:23:58 -0500

    add browserrouter

--- src/client.js
+++ src/client.js
@@ -1,9 +1,12 @@
 import App from "./App";
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
