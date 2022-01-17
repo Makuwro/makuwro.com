82e3c0a2e0ee345bd49bb43e885c555e3850ab5c
Christian Toney <christian@makuwro.com>
2022-01-17 11:08:55 -0500

    Create profile terms comp

---
+++ src/components/profile/ProfileTerms.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../styles/Profile.module.css";
+
+export default function ProfileTerms() {
+
+  return (
+    <section className={styles["profile-card"]} id={styles["profile-terms"]}>
+      This user didn't share any terms with Makuwro. If you want to commission them, or want to use their characters, art, or literature, you should <Link to="?message=">ask them about their policies</Link> before doing so.
+    </section>
+  );
+
+}
