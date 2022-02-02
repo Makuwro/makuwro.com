6bd09b63980af3b08a955ff50724e33597231681
Christian Toney <christian@makuwro.com>
2022-02-02 00:17:35 -0500

    Move button inside section

--- src/components/profile/ProfileTerms.js
+++ src/components/profile/ProfileTerms.js
@@ -7,10 +7,8 @@ export default function ProfileTerms() {
   return (
     <>
       <section className={styles["profile-card"]} id={styles["profile-terms"]}>
-        This user didn't share any terms with Makuwro. If you want to commission them, or want to use their characters, art, or literature, you should <Link to="?message=">ask them about their policies</Link> before doing so.
-      </section>
-      <section className={styles["profile-card"]} style={{width: "auto", marginTop: "-3rem"}}>
-        <button>Edit terms</button>
+        <p>This user didn't share any terms with Makuwro. If you want to commission them, or want to use their characters, art, or literature, you should <Link to="?message=">ask them about their policies</Link> before doing so.</p>
+        <button style={{display: "block", marginTop: "1rem"}}>Edit terms</button>
       </section>
     </>
   );
