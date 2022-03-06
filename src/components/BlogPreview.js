f8a904cfdbbdcf1ea50609042bbf49563451693c
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2022-03-05 23:49:17 -0500

    update

--- src/components/BlogPreview.js
+++ src/components/BlogPreview.js
@@ -1,9 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function BlogPreview({owner, content, coverURL}) {
 
   return (
-    
+    <section>
+
+    </section>
   );
 
 }
+
+BlogPreview.propTypes = {
+  owner: PropTypes.object.isRequired,
+  content: PropTypes.string,
+  coverURL: PropTypes.string
+};
