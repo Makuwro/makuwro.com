6aa9ca3fd887a05b2bcd50724dea42ac86212c3c
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2022-07-18 16:07:05 -0400

    conditionally open art viewer

--- src/components/library/ArtViewer.js
+++ src/components/library/ArtViewer.js
@@ -2,6 +2,7 @@ import styles from "../../styles/ArtViewer.module.css";
 import React, { useEffect, useState } from "react";
 import Collaborator from "../Collaborator";
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 
 const artRegex = /^\/(?<username>[^/]+)\/art\/(?<slug>[^/]+)\/?$/gm;
 
@@ -10,60 +11,65 @@ export default function ArtViewer({client}) {
   const [artInfo, setArtInfo] = useState();
   const [collaboratorComponents, setCollaboratorComponents] = useState([]);
   const [tagComponents, setTagComponents] = useState([]);
+  const location = useLocation();
 
   useEffect(() => {
+
     // Check if we're looking for an art piece.
+    if (artRegex.test(location.pathname)) {
 
-    // Get art from the server.
-    try {
+      try {
 
-      const art = {
-        imagePath: "https://upload.wikimedia.org/wikipedia/commons/5/55/Smokestacks_of_Chemical_Plant_11-1972_%283703566787%29.jpg",
-        description: "Aren't they a cutie? 🥰",
-        owner: {
-          username: "Christian",
-          displayName: "Christian Toney",
-          id: "6276f5c654cb1a7c52b5a7e2",
-          avatarPath: "https://pbs.twimg.com/profile_images/1503878170642104320/hWzBPHQl_400x400.jpg"
-        },
-        uploadedOn: "May 14, 2022",
-        tags: ["pollution", "your mom"]
-      };
-
-      // Iterate through each collaborator.
-      const collaborators = [art.owner];
-      for (let i = 0; collaborators.length > i; i++) {
-
-        // Create a component for each collaborator.
-        const {displayName, username, avatarPath, title} = collaborators[i];
-        collaborators[i] = <Collaborator username={username} displayName={displayName} avatar={avatarPath} title={title} />
-
+        // Get art from the server.
+        const art = {
+          imagePath: "https://upload.wikimedia.org/wikipedia/commons/5/55/Smokestacks_of_Chemical_Plant_11-1972_%283703566787%29.jpg",
+          description: "Aren't they a cutie? 🥰",
+          owner: {
+            username: "Christian",
+            displayName: "Christian Toney",
+            id: "6276f5c654cb1a7c52b5a7e2",
+            avatarPath: "https://pbs.twimg.com/profile_images/1503878170642104320/hWzBPHQl_400x400.jpg"
+          },
+          uploadedOn: "May 14, 2022",
+          tags: ["pollution", "your mom"]
+        };
+  
+        // Iterate through each collaborator.
+        const collaborators = [art.owner];
+        for (let i = 0; collaborators.length > i; i++) {
+  
+          // Create a component for each collaborator.
+          const {displayName, username, avatarPath, title} = collaborators[i];
+          collaborators[i] = <Collaborator username={username} displayName={displayName} avatar={avatarPath} title={title} />
+  
+        }
+  
+        // Now iterate through each tag.
+        const {tags} = art;
+        for (let i = 0; tags.length > i; i++) {
+  
+          tags[i] = <span>{tags[i]}</span>;
+  
+        }
+  
+        // Set the new tag components.
+        setTagComponents(tags);
+  
+        // Set the new collaborator components.
+        setCollaboratorComponents(collaborators);
+  
+        // We're ready to show what we got!
+        setArtInfo(art);
+  
+      } catch (err) {
+  
+  
+  
       }
-
-      // Now iterate through each tag.
-      const {tags} = art;
-      for (let i = 0; tags.length > i; i++) {
-
-        tags[i] = <span>{tags[i]}</span>;
-
-      }
-
-      // Set the new tag components.
-      setTagComponents(tags);
-
-      // Set the new collaborator components.
-      setCollaboratorComponents(collaborators);
-
-      // We're ready to show what we got!
-      setArtInfo(art);
-
-    } catch (err) {
-
-
 
     }
 
-  }, []);
+  }, [location]);
 
   return artInfo ? (
     <section id={styles.viewer}>
