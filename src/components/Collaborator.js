0bad5cceaee4c32e6762aaa1cc620dee4e875d4f
Christian Toney <christian@makuwro.com>
2022-07-19 11:44:41 -0400

    use cdn for avatars

--- src/components/Collaborator.js
+++ src/components/Collaborator.js
@@ -1,11 +1,11 @@
 import styles from "../styles/ArtViewer.module.css";
 import React from "react";
 
-export default function Collaborator({avatar, displayName, username, title}) {
+export default function Collaborator({avatarPath, displayName, username, title}) {
 
   return (
     <a href={`/${username}`} className={styles.collaborator}>
-      <img className={styles.avatar} src={avatar} />
+      <img className={styles.avatar} src={`https://cdn.makuwro.com/${avatarPath}`} />
       <section className={styles.collaboratorText}>
         <section>
           {displayName || `@${username}`}
