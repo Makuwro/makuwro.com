158b342248d8722a0730b1d22417829f9a4b152b
Christian Toney <christian@makuwro.com>
2022-07-19 11:44:03 -0400

    add age restriction

--- src/components/library/ArtViewer.js
+++ src/components/library/ArtViewer.js
@@ -4,152 +4,177 @@ import Collaborator from "../Collaborator";
 import PropTypes from "prop-types";
 import { useLocation, useNavigate } from "react-router-dom";
 import Popup from "../popups/Popup";
-
-const artRegex = /^\/(?<username>[^/]+)\/art\/(?<slug>[^/]+)\/?$/gm;
+import { UnderageError } from "makuwro-errors";
 
 export default function ArtViewer({client}) {
 
   const [artInfo, setArtInfo] = useState();
   const [ageRestricted, setAgeRestricted] = useState(false);
-  const [warningDismissed, setWarningDismissed] = useState(true);
+  const [contentWarning, setContentWarning] = useState();
   const [collaboratorComponents, setCollaboratorComponents] = useState([]);
   const [tagComponents, setTagComponents] = useState([]);
-  const location = useLocation();
+  const {pathname} = useLocation();
+  const [username, setUsername] = useState();
 
   useEffect(() => {
 
-    // Check if we're looking for an art piece.
-    if (artRegex.test(location.pathname)) {
+    (async () => {
 
-      try {
+      // Check if we're looking for an art piece.
+      const artRegex = /^\/(?<username>[^/]+)\/art\/(?<slug>[^/]+)\/?$/gm;
+      const [match] = [...pathname.matchAll(artRegex)];
+      if (match) {
 
-        // Get art from the server.
-        const art = {
-          imagePath: "https://upload.wikimedia.org/wikipedia/commons/5/55/Smokestacks_of_Chemical_Plant_11-1972_%283703566787%29.jpg",
-          description: "Aren't they a cutie? 🥰",
-          owner: {
-            username: "Christian",
-            displayName: "Christian Toney",
-            id: "6276f5c654cb1a7c52b5a7e2",
-            avatarPath: "https://pbs.twimg.com/profile_images/1503878170642104320/hWzBPHQl_400x400.jpg"
-          },
-          uploadedOn: "May 14, 2022",
-          tags: ["pollution", "your mom"]
-        };
-  
-        // Iterate through each collaborator.
-        const collaborators = [art.owner];
-        for (let i = 0; collaborators.length > i; i++) {
-  
-          // Create a component for each collaborator.
-          const {displayName, username, avatarPath, title} = collaborators[i];
-          collaborators[i] = <Collaborator username={username} displayName={displayName} avatar={avatarPath} title={title} />
-  
+        try {
+
+          // Get the owner username and slug.
+          const {groups: {username, slug}} = match;
+          setUsername(username);
+
+          // Get art from the server.
+          const art = await client.getArt(username, slug);
+    
+          // Iterate through each collaborator.
+          const collaborators = [art.owner];
+          for (let i = 0; collaborators.length > i; i++) {
+    
+            // Create a component for each collaborator.
+            const {id, displayName, username, title} = collaborators[i];
+            collaborators[i] = <Collaborator username={username} displayName={displayName} avatarPath={`${id}/avatar`} title={title} />
+    
+          }
+    
+          // Now iterate through each tag.
+          const tags = art.tags || [];
+          for (let i = 0; tags.length > i; i++) {
+    
+            tags[i] = <span>{tags[i]}</span>;
+    
+          }
+    
+          // Set the new tag components.
+          setTagComponents(tags);
+    
+          // Set the new collaborator components.
+          setCollaboratorComponents(collaborators);
+
+          // Set the content warning.
+          setContentWarning(art.contentWarning);
+    
+          // We're ready to show what we got!
+          setArtInfo(art);
+
+          // Change the document title.
+          document.title = `${art.owner.displayName} (${art.owner.username}): ${art.description || "Undescribed art"}`;
+    
+        } catch (err) {
+    
+          if (err instanceof UnderageError) {
+
+            setAgeRestricted(true);
+
+          } else {
+
+            console.log(err);
+
+          }
+    
         }
-  
-        // Now iterate through each tag.
-        const {tags} = art;
-        for (let i = 0; tags.length > i; i++) {
-  
-          tags[i] = <span>{tags[i]}</span>;
-  
-        }
-  
-        // Set the new tag components.
-        setTagComponents(tags);
-  
-        // Set the new collaborator components.
-        setCollaboratorComponents(collaborators);
-  
-        // We're ready to show what we got!
-        setArtInfo(art);
 
-        // Change the document title.
-        document.title = `${art.owner.displayName} (${art.owner.username}): ${art.description || "Undescribed art"}`;
-  
-      } catch (err) {
-  
-        console.log(err);
-  
+      } else {
+
+        setAgeRestricted(false);
+        setArtInfo();
+
       }
 
-    } else {
+    })();
 
-      setArtInfo();
-
-    }
-
-  }, [location]);
+  }, [pathname]);
 
   const navigate = useNavigate();
   function close() {
     
     // Go back to the user's profile.
-    navigate(`/${artInfo.owner.username}/art`);
+    navigate(`/${username}/art`);
 
   }
 
-  return artInfo ? (
-    <section id={styles.viewer} onClick={close}>
-      {!warningDismissed && (
-        <Popup className={styles.warning} 
-          title="Content warning"
-          options={
-            <>
-              <button>Go back</button>
-              <button className="destructive">View anyway</button>
-            </>
-          }>
-          <section className="info">This content may not be appropriate for all ages. Viewer discretion is advised.</section>
-        </Popup>
-      )}
-      {ageRestricted && (
-        <Popup 
-          title="Age restricted"
-          options={
-            <button>Go back</button>
-          }>
-          You are not old enough to view this content.
-        </Popup>
-      )}
-      <section id={styles.imageContainer} onClick={(event) => event.stopPropagation()}>
-        <img src={artInfo.imagePath} />
-      </section>
-      <section id={styles.details} onClick={(event) => event.stopPropagation()}>
-        <section id={styles.collaborators}>
-          {collaboratorComponents}
-          {
-            collaboratorComponents[3] && (
-              <button>View all collaborators</button>
-            )
-          }
+  if (artInfo) {
+
+    return (
+      <section id={styles.viewer} onClick={close}>
+        {contentWarning && (
+          <Popup className={styles.warning} 
+            title="Content warning"
+            options={
+              <>
+                <button onClick={close}>Go back</button>
+                <button className="destructive" onClick={() => setContentWarning()}>View anyway</button>
+              </>
+            }>
+            {contentWarning}
+            {artInfo.ageRestrictionLevel > 0 && (
+              <section className="info">This content may not be appropriate for all ages. Viewer discretion is advised.</section>
+            )}
+          </Popup>
+        )}
+        <section id={styles.imageContainer} onClick={(event) => event.stopPropagation()}>
+          <img src={`https://cdn.makuwro.com/${artInfo.imagePath}`} />
         </section>
-        <section id={styles.description}>
-          {artInfo.description}
-          <section id={styles.metadata}>
-            <section id={styles.tags}>
-              {tagComponents}
-            </section>
-            <section id={styles.date}>
-              Uploaded on {artInfo.uploadedOn}
+        <section id={styles.details} onClick={(event) => event.stopPropagation()}>
+          <section id={styles.collaborators}>
+            {collaboratorComponents}
+            {
+              collaboratorComponents[3] && (
+                <button>View all collaborators</button>
+              )
+            }
+          </section>
+          <section id={styles.description}>
+            {artInfo.description}
+            <section id={styles.metadata}>
+              {tagComponents[0] && (
+                <section id={styles.tags}>
+                  {tagComponents}
+                </section>
+              )}
+              <section id={styles.date}>
+                Uploaded on {artInfo.uploadedOn}
+              </section>
             </section>
           </section>
-        </section>
-        <section id={styles.actions}>
-          {
-            client.user?.id === artInfo.owner.id && (
-              <button id={styles.edit}>Edit</button>
-            )
-          }
-          <button id={styles.like}>Like</button>
-          <button className="destructive" id={styles.report}>Report</button>
-        </section>
-        <section>
-          Comments disabled
+          <section id={styles.actions}>
+            {
+              client.user?.id === artInfo.owner.id && (
+                <button id={styles.edit}>Edit</button>
+              )
+            }
+            <button id={styles.like}>Like</button>
+            <button className="destructive" id={styles.report}>Report</button>
+          </section>
+          <section>
+            Comments disabled
+          </section>
         </section>
       </section>
-    </section>
-  ) : null;
+    );
+
+  } else if (ageRestricted) {
+
+    return (
+      <Popup 
+        title="Age restricted"
+        options={
+          <button onClick={close}>Go back</button>
+        }>
+        You are not old enough to view this content.
+      </Popup>
+    );
+
+  }
+
+  return null;
 
 }
 
